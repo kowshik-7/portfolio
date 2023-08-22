440ab68a9a4d8f3b93e9452f04bffb5ab18aec2f
kowshik-7 <kowshiksiva007@gmail.com>
2023-08-23 00:25:00 +0530

    third commit

--- home/index.js
+++ home/index.js
@@ -1,5 +1,3 @@
-window.alert("hello");
-
 
 var btn1 = document.getElementById("btn1");
 var btn2 = document.getElementById("btn2");
@@ -13,50 +11,92 @@ var page4 = document.getElementById("page4");
 // if ( $(window).width() > 739) {
     
 //     }
+// function callFunction1(){
+//     if ( $(window).width() > 600) {
+//         if(page1.style.display === "none"){
+//             page1.classList.add("active");
+//             page1.style.display="block";
+//             page2.style.display="none";
+//             page3.style.display="none";
+//             page4.style.display="none";
+    
+//         }
+//         else{
+//             page1.classList.add("active")
+//         }
+//     }
+    
+// }
+// function callFunction2(){
+//     if ( $(window).width() > 600) {
+//         page2.classList.add("active");
+//         page2.style.display="block";
+//         page1.style.display="none";
+//         page3.style.display="none";
+//         page4.style.display="none";
+//     }
+    
+
+// }
+// function callFunction3(){
+//     if ( $(window).width() > 600) {
+//         page3.classList.add("active");
+//         page3.style.display="block";
+//         page1.style.display="none";
+//         page2.style.display="none";
+//         page4.style.display="none";
+//     }
+    
+// }
+// function callFunction4(){
+//     if ( $(window).width() > 600) {
+//         page4.classList.add("active");
+//         page4.style.display="block";
+//         page1.style.display="none";
+//         page2.style.display="none";
+//         page3.style.display="none";
+//     }
+    
+// }
+
+// // if ( $(window).width() > 600){
+// //     document.getElementById("btn1").style.backgroundColor="red";
+// // }
+
+
+
 function callFunction1(){
-    if ( $(window).width() > 600) {
-        if(page1.style.display === "none"){
-            page1.classList.add("active");
-            page1.style.display="block";
-            page2.style.display="none";
-            page3.style.display="none";
-            page4.style.display="none";
-    
-        }
-        else{
-            page1.classList.add("active")
-        }
-    }
-    
-}
-function callFunction2(){
-    if ( $(window).width() > 600) {
-        page2.classList.add("active");
-        page2.style.display="block";
-        page1.style.display="none";
+    if(page1.style.display === "none"){
+        page1.classList.add("active");
+        page1.style.display="block";
+        page2.style.display="none";
         page3.style.display="none";
         page4.style.display="none";
+
     }
-    
+    else{
+        page1.classList.add("active")
+    }
+}
+function callFunction2(){
+    page2.classList.add("active");
+    page2.style.display="block";
+    page1.style.display="none";
+    page3.style.display="none";
+    page4.style.display="none";
 
 }
 function callFunction3(){
-    if ( $(window).width() > 600) {
-        page3.classList.add("active");
-        page3.style.display="block";
-        page1.style.display="none";
-        page2.style.display="none";
-        page4.style.display="none";
-    }
-    
+    page3.classList.add("active");
+    page3.style.display="block";
+    page1.style.display="none";
+    page2.style.display="none";
+    page4.style.display="none";
 }
 function callFunction4(){
-    if ( $(window).width() > 600) {
-        page4.classList.add("active");
-        page4.style.display="block";
-        page1.style.display="none";
-        page2.style.display="none";
-        page3.style.display="none";
-    }
-    
+    page4.classList.add("active");
+    page4.style.display="block";
+    page1.style.display="none";
+    page2.style.display="none";
+    page3.style.display="none";
 }
