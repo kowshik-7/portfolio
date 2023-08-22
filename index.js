66b280f52dbec855f21f53ab7125fcf7e2f134ec
kowshik-7 <kowshiksiva007@gmail.com>
2023-08-22 18:17:44 +0530

    first commit

--- index.js
+++ index.js
@@ -8,39 +8,53 @@ var page1 = document.getElementById("page1");
 var page2 = document.getElementById("page2");
 var page3 = document.getElementById("page3");
 var page4 = document.getElementById("page4");
-
+// if ( $(window).width() > 739) {
+    
+//     }
 function callFunction1(){
-    if(page1.style.display === "none"){
-        page1.classList.add("active");
-        page1.style.display="block";
-        page2.style.display="none";
-        page3.style.display="none";
-        page4.style.display="none";
-
+    if ( $(window).width() > 600) {
+        if(page1.style.display === "none"){
+            page1.classList.add("active");
+            page1.style.display="block";
+            page2.style.display="none";
+            page3.style.display="none";
+            page4.style.display="none";
+    
+        }
+        else{
+            page1.classList.add("active")
+        }
     }
-    else{
-        page1.classList.add("active")
-    }
+    
 }
 function callFunction2(){
-    page2.classList.add("active");
-    page2.style.display="block";
-    page1.style.display="none";
-    page3.style.display="none";
-    page4.style.display="none";
+    if ( $(window).width() > 600) {
+        page2.classList.add("active");
+        page2.style.display="block";
+        page1.style.display="none";
+        page3.style.display="none";
+        page4.style.display="none";
+    }
+    
 
 }
 function callFunction3(){
-    page3.classList.add("active");
-    page3.style.display="block";
-    page1.style.display="none";
-    page2.style.display="none";
-    page4.style.display="none";
+    if ( $(window).width() > 600) {
+        page3.classList.add("active");
+        page3.style.display="block";
+        page1.style.display="none";
+        page2.style.display="none";
+        page4.style.display="none";
+    }
+    
 }
 function callFunction4(){
-    page4.classList.add("active");
-    page4.style.display="block";
-    page1.style.display="none";
-    page2.style.display="none";
-    page3.style.display="none";
+    if ( $(window).width() > 600) {
+        page4.classList.add("active");
+        page4.style.display="block";
+        page1.style.display="none";
+        page2.style.display="none";
+        page3.style.display="none";
+    }
+    
 }
